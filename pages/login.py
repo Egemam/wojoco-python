1ac4c5962850ad2deb85b4cb2c26ec5758c15a9d
Egemam <destekbekleniyor009@gmail.com>
2024-12-17 22:43:41 +0300

    test5

--- pages/login.py
+++ pages/login.py
@@ -7,13 +7,10 @@ controller = CookieController()
 st.title("Login")
 
 def login(username, password):
-    print("test")
-    #print(controller.get('token'))
-    #controller.set('token', base64.b64encode(username.encode("ascii"))+base64.b64encode(password.encode("ascii")))
-    #print(controller.get('token'))
+    print(controller.get('token'))
+    controller.set('token', base64.b64encode(username.encode("ascii"))+base64.b64encode(password.encode("ascii")))
+    print(controller.get('token'))
 
 username = st.text_input("Username", placeholder="JohnDoe")
 password = st.text_input("Password", placeholder="*******", type="password")
 button = st.button("Login", on_click=login(username,password))
-if button:
-    print("test")
