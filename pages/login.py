e2605dc18937984ff10fbd4cca05d7413feb55bb
Egemam <destekbekleniyor009@gmail.com>
2024-12-17 22:57:06 +0300

    reads the csv file

--- pages/login.py
+++ pages/login.py
@@ -1,14 +1,20 @@
 import streamlit as st
 from streamlit_cookies_controller import CookieController
 import base64
+import csv
 
 controller = CookieController()
 
 st.title("Login")
 
 def login(username, password):
-    print(controller.get('token'))
-    controller.set('token', base64.b64encode(username.encode("ascii")).decode("ascii")+"."+base64.b64encode(password.encode("ascii")).decode("ascii"))
+    token = base64.b64encode(username.encode("ascii")).decode("ascii")+"."+base64.b64encode(password.encode("ascii")).decode("ascii")
+    print(token)
+    with open('userlist.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        if token in [row['token'] for row in reader]:
+            print("Login successful")
+    controller.set('token', token)
     print(controller.get('token'))
 
 username = st.text_input("Username", placeholder="JohnDoe")
