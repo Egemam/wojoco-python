a9abe9e1ea1def52571b24ec2e2beaaf906f1f25
Egemam <destekbekleniyor009@gmail.com>
2024-12-17 22:36:38 +0300

    can now manage cookies

--- pages/login.py
+++ pages/login.py
@@ -1,4 +1,16 @@
 import streamlit as st
+from streamlit_cookies_controller import CookieController
+import base64
+
+controller = CookieController()
+
+st.title("Login")
+
+def login(username, password):
+    controller.set('token', base64.b64encode(username)+base64.b64encode(password))
 
 username = st.text_input("Username", placeholder="JohnDoe")
 password = st.text_input("Password", placeholder="*******", type="password")
+button = st.button("Login", on_click=login(username,password))
+
+print(controller.get('token'))
