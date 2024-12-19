dce4e679f7759113860065d6d89ec4652b7ea948
Egemam <destekbekleniyor009@gmail.com>
2024-12-19 22:27:12 +0300

    test

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import streamlit as st
 import csv
 from streamlit_cookies_controller import CookieController
 
-controller = CookieController()
-
 #st.button("Login",on_click=st.switch_page("pages/login.py"))
 with open('userlist.csv', newline='') as csvfile:
+    controller = CookieController()
     reader = csv.DictReader(csvfile)
     if controller.get('token') in [row['token'] for row in reader]:
         logout_button = st.button("Logout",on_click=controller.delete('token'))
