922ba30fead78f3b4ca6ffe0d1ee2b9b8636ff33
Egemam <destekbekleniyor009@gmail.com>
2024-12-19 20:24:21 +0300

    added autoredirect to the login page

--- pages/Login.py
+++ pages/Login.py
@@ -5,7 +5,11 @@ import csv
 
 controller = CookieController()
 
-st.title("Login")
+with open('userlist.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        if token in [row['token'] for row in reader]:
+            st.switch_page("main.py")
+
 
 def login(username, password):
     token = base64.b64encode(username.encode("ascii")).decode("ascii")+"."+base64.b64encode(password.encode("ascii")).decode("ascii")
