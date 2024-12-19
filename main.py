a03e366f109e6ebadd2a5fc2b8e855e091a26abd
Egemam <destekbekleniyor009@gmail.com>
2024-12-19 20:54:47 +0300

    test

--- main.py
+++ main.py
@@ -17,22 +17,4 @@ with open('userlist.csv', newline='') as csvfile:
 
 review_button = st.button("review",on_click=review_compare)
 
-'''if action == "1":
-    user = input("Enter your name: ")
-    text = input("Enter your portfolio: ")
-    portfolios.portfolio_submit(user, text)
-elif action == "2":
-    user = input("Enter your name: ")
-    place = input("Enter the place: ")
-    text = input("Enter your review: ")
-    reviews.review_submit(user, place, text)
-elif action == "3":
-    user = input("Enter your name: ")
-    place = input("Enter the place: ")    
-    print(reviews.compare_sum(user,place))
-elif action == "4":
-    print("Goodbye!")
-else:
-    print("Invalid action")'''
-
 #print(controller.get('token'))
