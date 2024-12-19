d812e478753a482c11bd2bdd585cdfe2156e1b6e
Egemam <destekbekleniyor009@gmail.com>
2024-12-19 21:01:31 +0300

    test

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from streamlit_cookies_controller import CookieController
 
 controller = CookieController()
 
+print(st.secrets["TOKEN"])
+
 #st.button("Login",on_click=st.switch_page("pages/login.py"))
 with open('userlist.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
