7817ef2ae5fabcf65e22b80287b828d454e55bc8
Egemam <destekbekleniyor009@gmail.com>
2024-12-19 23:09:28 +0300

    fix

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ with open('userlist.csv', newline='') as csvfile:
     if controller.get('token') in [row['token'] for row in reader]:
         logout_button = st.button("Logout",on_click=controller.delete('token'))
 def show_comparison():
-    text = eval(reviews.compare_sum("egemam","maya"))
-    result_icon = st.image(f"images/{text[0]}.png")
+    while 1:
+        try:
+            text = eval(reviews.compare_sum("egemam","maya"))
+            result_icon = st.image(f"images/{text[0]}.png")
+            break
+        except:
+            continue
     st.write(
         
     )
