d998f0791b7cabbd39cb52d8693fd9cfe22f1dc7
Egemam <destekbekleniyor009@gmail.com>
2024-12-19 20:49:43 +0300

    quick fix8

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ controller = CookieController()
 def review_compare():
     print(reviews.compare_sum("egemam","maya"))
 
+print("e")
 #st.button("Login",on_click=st.switch_page("pages/login.py"))
 with open('userlist.csv', newline='') as csvfile:
     print("e")
