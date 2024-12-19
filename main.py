33dd8289f512f2d576f98dfe62d0d05c971fe0b0
Egemam <destekbekleniyor009@gmail.com>
2024-12-19 20:56:19 +0300

    test

--- main.py
+++ main.py
@@ -6,15 +6,12 @@ from streamlit_cookies_controller import CookieController
 
 controller = CookieController()
 
-def review_compare():
-    print(reviews.compare_sum("egemam","maya"))
-
 #st.button("Login",on_click=st.switch_page("pages/login.py"))
 with open('userlist.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     if controller.get('token') in [row['token'] for row in reader]:
         logout_button = st.button("Logout",on_click=controller.delete('token'))
 
-review_button = st.button("review",on_click=review_compare)
+review_button = st.button("review",on_click=reviews.compare_sum("egemam","maya"))
 
 #print(controller.get('token'))
