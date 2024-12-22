b3470705984ea6a04ce14700e8db8a2673bcec37
Egemam <destekbekleniyor009@gmail.com>
2024-12-22 23:02:26 +0300

    writes portfolio

--- portfolios.py
+++ portfolios.py
@@ -1,5 +1,6 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import streamlit as st
 
 cluster = MongoClient(st.secrets["MONGODB_URI"], server_api=ServerApi('1'))
 db = cluster["wojoco"]
