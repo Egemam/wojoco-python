2a974bdccf423bb785b42fa97961c529d6cd99df
Egemam <destekbekleniyor009@gmail.com>
2024-12-22 21:19:56 +0300

    test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ text = st.text_area("Write your portfolio")
 def writetext():
     portfolios.portfolio_submit("egemam", text)
 def readtext():
-    portfolios.portfolio_read("egemam")
+    st.write(portfolios.portfolio_read("egemam"))
 # Function to read CSV and check login
 st.button("Write", on_click=writetext)
 st.button("Read", on_click=readtext)
