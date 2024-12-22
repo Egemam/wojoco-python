6a880991ca659cbab41fd80ef40d9757c73284a6
Egemam <destekbekleniyor009@gmail.com>
2024-12-22 19:23:20 +0300

    test

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def show_comparison():
         if not check:
             st.write("Please log in to access this page.")
             return 0
-        name = "".join(row['username'] for row in reader if row['token'] == controller.get('token'))
+        name = "".join(row['username'] for row in reader)
         while 1:
             try:
                 text = eval(reviews.compare_sum(name,"maya"))
