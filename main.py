4579dd2702bbbc8aed5942ea5c529e225f351a3d
Egemam <destekbekleniyor009@gmail.com>
2024-12-22 20:31:26 +0300

    test

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def show_comparison():
         if not check:
             st.write("Please log in to access this page.")
             return 0"""
+        name = "".join(row['username'] for row in reader)
         while 1:
             try:
-                name = "".join(row['username'] for row in reader)
                 st.write("name=" + name)
                 text = eval(reviews.compare_sum(name,"maya"))
                 result_icon = st.image(f"images/{text[0]}.png")
