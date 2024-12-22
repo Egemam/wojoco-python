79ab5dbbac0a8b4c0f4514a6a01111db17857059
Egemam <destekbekleniyor009@gmail.com>
2024-12-22 22:37:50 +0300

    automatically redirects to main page if logged in

--- pages/Register.py
+++ pages/Register.py
@@ -11,7 +11,7 @@ collection = db["userlist"]
 controller = CookieController()
 
 if controller.get('token'):
-    st.st.switch_page("main.py")
+    st.switch_page("main.py")
 
 def register(username, password):
     if not collection.find_one({"_id": username}):
