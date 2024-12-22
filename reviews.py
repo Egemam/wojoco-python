f827c57ed09c32d849b3a42b847b68cddeb31d92
Egemam <destekbekleniyor009@gmail.com>
2024-12-23 01:05:15 +0300

    bugfixes

--- reviews.py
+++ reviews.py
@@ -4,6 +4,7 @@ import streamlit as st
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import streamlit as st
+import portfolios
 
 cluster = MongoClient(st.secrets["MONGODB_URI"], server_api=ServerApi('1'))
 db = cluster["wojoco"]
