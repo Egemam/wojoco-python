d1076c9dbb2ad43ae37ec7b2bf0e02f5854187fc
Egemam <destekbekleniyor009@gmail.com>
2024-12-22 21:02:53 +0300

    test

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def show_comparison():
     while 1:  # Infinite loop with try-except
         try:
             text = eval(reviews.compare_sum(name, "maya"))  # Ensure `reviews.compare_sum` returns eval-safe data
-            print(text[0] + text[1] + text[2])  # Debug print
+            st.write(text[0] + text[1] + text[2])  # Debug print
             result_icon = st.image(f"images/{text[0]}.png")
             st.write("Pros:")
             st.write("\n".join(text[1]))
