eb0528d3ef0c81b1dba4f44ed45738c9795228e5
Egemam <destekbekleniyor009@gmail.com>
2024-12-22 21:18:21 +0300

    test

--- main.py
+++ main.py
@@ -10,5 +10,8 @@ controller = CookieController()
 text = st.text_area("Write your portfolio")
 def writetext():
     portfolios.portfolio_submit("egemam", text)
+def readtext():
+    portfolios.portfolio_read("egemam")
 # Function to read CSV and check login
 st.button("Write", on_click=writetext)
+st.button("Read", on_click=readtext)
--- portfolios.py
+++ portfolios.py
@@ -2,3 +2,7 @@ def portfolio_submit(user, text):
     path = f'portfolio/{user}.txt'
     with open(path, 'w') as fp:
         fp.write(text)
+def portfolio_read(user):
+    path = f'portfolio/{user}.txt'
+    with open(path, 'r') as fp:
+        fp.read(text)
