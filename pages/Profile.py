2d0c0b075efa053d0d3351b87584aec303b626e3
Egemam <destekbekleniyor009@gmail.com>
2024-12-22 23:12:01 +0300

    fixed the portfolio bug

--- pages/Profile.py
+++ pages/Profile.py
@@ -20,13 +20,6 @@ def is_logged_in():
 def get_username():
     return collection.find_one({"token": controller.get('token')})["_id"]
 
-#writes portfolio
-def writetext():
-    if not is_logged_in():
-        st.warning("Please log in to access this page.")
-        return
-    portfolios.portfolio_submit(collection.find_one({"token": controller.get('token')})["_id"], text)
-
 # Show comparison logic
 def show_comparison():
     # Verify user login
@@ -63,8 +56,15 @@ with st.sidebar:
     else:
         st.write("Please log in.")
 
-text = st.text_area("Write your portfolio",value=portfolios.portfolio_read(get_username()))
-# Function to read CSV and check login
+portfolio_text_area = st.text_area("Write your portfolio",value=portfolios.portfolio_read(get_username()))
+
+# Writes portfolio
+def writetext():
+    if not is_logged_in():
+        st.warning("Please log in to access this page.")
+        return
+    portfolios.portfolio_submit(collection.find_one({"token": controller.get('token')})["_id"], portfolio_text_area)
+
 st.button("Write", on_click=writetext)
 # Main button for showing the comparison
 st.button("Review", on_click=show_comparison)
